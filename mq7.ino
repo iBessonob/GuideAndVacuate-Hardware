aa73b406876a1c0d94a88b7d9cf48c1bc9eecdcd
Aabhas Senapati <aabhas2211@gmail.com>
2020-09-19 22:34:27 +0530

    Create mq7.ino

---
+++ mq7.ino
@@ -0,0 +1,22 @@
+
+float RS_gas = 0;
+float ratio = 0;
+float sensorValue = 0;
+float sensor_volt = 0;
+float R0 = 2450.0;
+ 
+void setup() {
+ Serial.begin(9600);
+}
+ 
+void loop() {
+   sensorValue = analogRead(34);
+   sensor_volt = (sensorValue*3.3)/4096;
+   RS_gas = (3.3-sensor_volt)/sensor_volt;
+   ratio = RS_gas/R0; //Replace R0 with the value found using the sketch above
+   float x = 1538.46 * ratio;
+   float ppm = pow(x,-1.709);
+   Serial.print("PPM: ");
+   Serial.println(ppm);
+   delay(1000);
+}
